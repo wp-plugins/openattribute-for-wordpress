534d02b32e617dd0a89f188f1d818ed14bb03e63
OpenAttribute <OpenAttribute@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-15 00:44:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/openattribute-for-wordpress/tags/0.991@475649 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- openattribute.php
+++ openattribute.php
@@ -4,7 +4,7 @@ Plugin Name: Open Attribute
 Plugin URI: http://openattribute.com
 OpenAttribute allows you to add licensing information to your Wordpress site and individual blogs. It places information into posts and RSS feeds as well as other user friendly features.
 Version: 0.991
-Author: Open Attribute, pgogy
+Author: OpenAttribute, pgogy
 Author URI: http://openattribute.com
 */
 
